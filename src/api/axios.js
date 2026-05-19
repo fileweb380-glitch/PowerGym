80937feadd71f9f223daf45c18575460d1f2cbbf
fileweb380-glitch <webfile380@gmail.com>
2026-05-19 20:19:35 +0300

    connected api

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = axios.create({
 
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://powergym-backend-1svk.onrender.com/api',
 
   headers: {
     'Content-Type': 'application/json'
