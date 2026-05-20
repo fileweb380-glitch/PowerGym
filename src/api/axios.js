8bd7b5886a6149d318ad5afecd4c53121cb9556a
fileweb380-glitch <webfile380@gmail.com>
2026-05-20 15:39:04 +0300

    fixed the axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -10,4 +10,24 @@ const API = axios.create({
 
 })
 
+// Automatically send token with every request
+API.interceptors.request.use(
+
+  (req) => {
+
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`
+    }
+
+    return req
+  },
+
+  (error) => {
+    return Promise.reject(error)
+  }
+
+)
+
 export default API
